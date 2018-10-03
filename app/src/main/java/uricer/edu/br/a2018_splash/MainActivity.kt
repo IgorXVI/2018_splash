a789e76ba64ce65b958a810947493cc8a63f8ccc
Alunos <inazumaseleven04@gmail.com>
2018-10-02 22:05:20 -0300

    KKKh2

--- app/src/main/java/uricer/edu/br/a2018_splash/MainActivity.kt
+++ app/src/main/java/uricer/edu/br/a2018_splash/MainActivity.kt
@@ -3,6 +3,8 @@ package uricer.edu.br.a2018_splash
 import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+import android.support.constraint.ConstraintSet
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,9 +12,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var c: ConstraintLayout = layout
         btn_rosario.setOnClickListener {
             var img = BitmapFactory.decodeResource(resources, R.drawable.rosario)
-
+            c.setBackgroundResource(R.drawable.rosario)
         }
         btn_esfaqueador.setOnClickListener {
             var img = BitmapFactory.decodeResource(resources, R.drawable.esfaqueador)
